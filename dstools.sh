8f6c2ef382e2fd687cd6bd16a9cfbc222a6a5819
xcovery45 <48662920+xcovery45@users.noreply.github.com>
2019-04-11 20:45:47 +0700

    Dsspam
    Only Noob

---
+++ dstools.sh
@@ -0,0 +1,44 @@
+toilet -f slant --gay "DS - spam"
+sleep 1
+echo "\033[36;1mCreated By: DeadSec Team"
+sleep 1
+echo "\033[36;1mAuthor By : FirmansyahKen/XOX758"
+sleep 1
+echo
+echo "\033[32;1mPilih spam yang ada dibawah berikut:"
+echo
+echo "\033[34;1m[1]. \033[36;1mTelkomsel"
+echo "\033[34;1m[0]. \033[31;1mKeluar"
+echo "\033[37;1mPilih angka :"
+read mrrm
+
+if 
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1m"
+toilet -f slant "Telkomsel"
+sleep 1
+echo "\033[31;1mCreated By : DeadSec"
+echo "\033[31;1mAuthor By  : FirmansyahKen/XOX758"
+sleep 4
+clear
+cd
+cd /sdcard
+cd MyTools
+cd tool
+php Telkomsel.php
+fi
+
+if 
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+sleep 1
+echo "\033[31;1mkeluar"
+sleep 1 
+echo
+echo "\033[32;1mTerima Kasih Sampai Jumpa Kembali :)"
+sleep 3
+clear
+echo "\033[37;1m"
+fi
