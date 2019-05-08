19ac0d6d084bb25a3e9ba0e2289939e887447ba6
xcovery45 <48662920+xcovery45@users.noreply.github.com>
2019-05-08 22:16:54 +0700

    S

---
+++ spam.sh
@@ -0,0 +1,24 @@
+clear
+figlet "Telkomsel Spam"
+sleep 2
+echo "Author By: Vcxz33"
+sleep 1
+echo
+echo "Apakah mau lanjut"
+echo "ketik y/n untuk ya dan tidak"
+read mrrm
+
+if 
+[ $mrrm = y ] || [ $mrrm = y ]
+then
+clear
+sleep 1
+toilet -f slant --gay "Telkomsel"
+sleep 2
+cd tool
+php telkomsel.php
+echo "ketik ctrl+c untuk exit"
+python2 readme.md
+clear
+
+fi
